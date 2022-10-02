205d5218955d2ef75fe8530d8423efe70ecb6983
Italo Magno <italomagno10@gmail.com>
2022-10-02 19:28:14 -0300

    commit multer
    foi instalado o multer e feito o primeiro download de arquivo e jogado numa pasta chamada tmp, fora da raiz do projeto

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoiresController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+  dest: "./tmp"
+})
 
 const categoriesRoutes = Router();
 
@@ -12,5 +17,14 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
  return listCategoiresController.handle(request,response)
 });
+
+categoriesRoutes.post("/import", upload.single("file"), (request,response)=>{
+
+  const {file} = request;
+  console.log(file)
+
+
+  return response.send()
+})
 
 export { categoriesRoutes };
