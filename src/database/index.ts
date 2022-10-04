9b4d8ff884aa7c289dd3351c08229db4fcf3d9d2
Italo Magno <italomagno10@gmail.com>
2022-10-04 20:14:00 -0300

    commit creating postgres container

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,13 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from 'typeorm';
 
+interface IOptions {
+  host: string;
+}
 
-createConnection()
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
